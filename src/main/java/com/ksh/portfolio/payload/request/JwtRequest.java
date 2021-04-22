35c542420125f07c660d58d6ce5dbaecef0d4fb0
kimsunhak <kkyj3954@ifcommunity.co.kr>
2021-04-22 18:35:53 +0900

    오늘도 1일 1커밋이다 ^__^

--- src/main/java/com/ksh/portfolio/payload/request/JwtRequest.java
+++ src/main/java/com/ksh/portfolio/payload/request/JwtRequest.java
@@ -13,7 +13,6 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @Builder
 public class JwtRequest {
-
     @ApiParam(value = "jwt 토큰", required = true)
     @NotBlank(message = "jwt 토큰이 비어있습니다.")
     private String jwt;
--- src/main/java/com/ksh/portfolio/security/oauth2/user/OAuth2UserInfoFactory.java
+++ src/main/java/com/ksh/portfolio/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -1,7 +1,9 @@
 package com.ksh.portfolio.security.oauth2.user;
 
-import com.ifcommunity.corona.domain.member.AuthProvider;
-import com.ifcommunity.corona.exception.OAuth2AuthenticationProcessingException;
+
+
+import com.ksh.portfolio.domain.member.AuthProvider;
+import com.ksh.portfolio.exception.OAuth2AuthenticationProcessingException;
 
 import java.util.Map;
 
@@ -9,7 +11,7 @@ public class OAuth2UserInfoFactory {
 
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
         if (registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
-            return new com.ifcommunity.corona.security.oauth2.user.GoogleOAuth2UserInfo(attributes);
+            return new GoogleOAuth2UserInfo(attributes);
         } else {
             throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
         }
