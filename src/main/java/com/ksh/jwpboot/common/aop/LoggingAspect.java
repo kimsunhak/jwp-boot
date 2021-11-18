f295673d181f35bf3db5dec1f575c4f4d49d0f89
kimsunhak <kkyj3954@naver.com>
2021-11-18 17:30:52 +0900

    ** TimeAop 적용

---
+++ src/main/java/com/ksh/jwpboot/common/aop/LoggingAspect.java
@@ -0,0 +1,28 @@
+package com.ksh.jwpboot.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Around("execution(* com.ksh.jwpboot..*Controller.*(..))")
+    public Object timeTrace(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            log.info("{} : {}", joinPoint.getSignature().getName(), timeMs / 1000.0 + " MS");
+        }
+    }
+}
+
